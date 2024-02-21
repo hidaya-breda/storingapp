d5dd072887b207fe869ed886e8cdaaae4f1e2818
d304219 <d304219@edu.curio.nl>
2024-02-21 10:06:41 +0100

    update

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -2,16 +2,41 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+$type = $_POST['type'];
 $capaciteit = $_POST['capaciteit']; 
-$melder = $_POST['melder'];
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = true;
+}
+else
+{
+    $prioriteit = false;
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+}
+$melder = $_POST['melder'];
+$overig = $_POST['overig'];
+
+// echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
-require_once 'conn.php';
+require_once 'C:\laragon\www\storingapp\config\conn.php';
 
 //2. Query
+$query="INSERT INTO meldingen(attractie, type, capaciteit, prioriteit, melder, overige_info)VALUES(:attractie,:type,:capaciteit,:prioriteit,:melder,:overige_info)";
 
 //3. Prepare
+$statement=$conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie"=>$attractie,
+    ":type"=>$type,
+    ":capaciteit"=>$capaciteit,
+    ":prioriteit"=>$prioriteit,
+    ":melder"=>$melder,
+    ":overige_info"=>$overig
+]);
+
+$items=$statement->fetchAll(PDO::FETCH_ASSOC);
+
+header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -22,7 +22,21 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type" id="type">
+                    <option value="">- kies een type -</option>
+                    <option value="Achtbaan">Achtbaan</option>
+                    <option value="Draaiende attractie">Draaiende attractie</option>
+                    <option value="Kinderattractie">Kinderattractie</option>
+                    <option value="Restaurant, cafe, etc.">Restaurant, cafe, etc.</option>
+                    <option value="Parkshow">Parkshow</option>
+                    <option value="Waterattractie">Waterattractie</option>
+                    <option value="Overig">Overig</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="prioriteit">PRIO</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">Melding met prioriteit</label>
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
@@ -31,6 +45,11 @@
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+
+            <div class="form-group">
+                <label for="overig">Overige info</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
 
             <input type="submit" value="Verstuur melding">
